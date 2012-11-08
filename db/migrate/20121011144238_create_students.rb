459150161337c547c03bcc55a755a16a8c930f52
Apprentice <apprentice@devbootcamp.com>
2012-11-08 09:47:56 -0800

    Submit Migrations for Schema Creation

--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -1,9 +1,17 @@
 require_relative '../config'
 
-# this is where you should use an ActiveRecord migration to 
+# this is where you should use an ActiveRecord migration to
 
 class CreateStudents < ActiveRecord::Migration
   def change
     # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :students do |s|
+      s.string :first_name
+      s.string :last_name
+      s.string :gender
+      s.string :email
+      s.string :phone
+      s.date :birthday
+    end
   end
 end
