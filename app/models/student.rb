8eb2690799e07c054a5d3b9ec382a6c34a999a73
Apprentice <apprentice@devbootcamp.com>
2012-11-08 11:54:44 -0800

    Completed Your First Object-Relational Model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,14 @@
 require_relative '../../db/config'
+#include datetime
+
+class Student < ActiveRecord::Base
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def age
+    ((Date.today - self.birthday)/365.25).to_i
+  end
+end
 
 # implement your Student model here
